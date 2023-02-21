3b72530dee401d3b3359ac2b48f0590a62b87aca
Clover <clover.hsc@gmail.com>
2023-02-21 14:30:10 +0800

    1. Setup OpenAI API environment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OpenAIComponent } from './open-ai/open-ai.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenAIComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
